c1edcc8a8ac14a0a168c7c8072e087e4bbf4dbcb
Asmaa <asmaa.abuhammad@gmail.com>
2023-01-12 07:58:34 +0200

    Add first file

---
+++ src/com/company/Main.java
@@ -0,0 +1,33 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        Addition add = new Addition();
+        System.out.println(add.calc(5,6));
+
+
+
+    }
+
+
+    public interface Operations{
+
+        int a = 0;
+        int b =0;
+
+        public abstract int calc(int a, int b);
+    }
+
+
+    public static class Addition implements Operations{
+
+
+        @Override
+        public int calc(int a, int b) {
+            return a+b;
+
+        }
+    }
+
+
+}
